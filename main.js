87fb447ecd9c358fcb13fe3427c689699d831ae5
Narendra <scriptstar@hotmail.co.uk>
2012-10-15 11:06:38 +0100

    Initial commit
    This is an advanced backbone.js tutorial application that uses HTML5,
    jQuery and backbone.js. This is not a typical "Hello world" application.
    You will learn the backbone.js in lightening quick speed with this
    tutorial. Inside this tutorial, we are building a real time exam
    question and answer style application. After you finish this tutorial,
    you will get the black belt status in backbone.js. Download now to see
    how you can master this latest technology in days not in weeks or
    months. Please visit my blog @ www.backboneblackbelt.com that is
    associated with this tutorial for more detailed information and in depth
    explanation.

---
+++ main.js
@@ -0,0 +1,29 @@
+﻿var examQuestions = new ExamQuestions();
+
+examQuestions.fetch({
+
+    success: function () {
+        var questionList = examQuestions.toJSON();
+        for (var key in questionList[0].questions) {
+            var x = questionList[0].questions[key].question;
+
+
+            for (var key2 in questionList[0].questions[key].choices) {
+                var a = questionList[0].questions[key].choices[key2].a;
+                var b = questionList[0].questions[key].choices[key2].b;
+                var c = questionList[0].questions[key].choices[key2].c;
+                var d = questionList[0].questions[key].choices[key2].d;
+
+                var ans = "<ul>";
+                ans = ans + "<li>" + a + "</li><li>" + b + "</li><li>" + c + "</li><li>" + d + "</li>";
+                ans = ans + "</ul>";
+            }
+
+        }
+
+        $('#questiondiv').append(x);
+        $('#answerDiv').append(ans);
+    }
+});
+
+
---
+++ models/Exam.js
@@ -0,0 +1,9 @@
+﻿var Exam = Backbone.Model.extend({})
+
+var ExamQuestions = Backbone.Collection.extend({
+    model: Exam,
+
+    //url: 'http://dl.dropbox.com/u/5152028/test.json'
+    url: 'http://localhost:1858/boilerplate/test.json'
+
+})
